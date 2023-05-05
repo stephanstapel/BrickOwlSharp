53689392a7852eb6a13e99cf370ef8248758c018
Stephan <stephan.stapel@web.de>
2023-05-05 18:06:38 +0200

    Update Program.cs

--- BrickOwlSharp.Demos/Program.cs
+++ BrickOwlSharp.Demos/Program.cs
@@ -5,7 +5,7 @@ internal static class Program
 {
     static async Task<int> Main()
     {
-        BrickOwlClientConfiguration.Instance.ApiKey = "9c61000326a7ff76fc79becb739e7431d1d23586f7841db68d4a681f05c164ec";
+        BrickOwlClientConfiguration.Instance.ApiKey = "----";
 
         /*
         WishlistDemo demo = new WishlistDemo();
